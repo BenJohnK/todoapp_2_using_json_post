f4d21d6c7ff26dadc767c9452817ecb2f06d49a6
Ben <benjohn.mec@gmail.com>
2021-02-25 17:26:15 +0530

    adding new

--- todo/views.py
+++ todo/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request,"todo/index.html",{}) #commentedss
+    return render(request,"todo/index.html",{}) 
