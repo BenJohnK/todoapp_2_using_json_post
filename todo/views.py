f5f6cd1753c22e9ac8874bf8173bbb8ecb4c9250
Ben <benjohn.mec@gmail.com>
2021-02-26 08:40:48 +0530

    adding new files

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
-    return render(request,"todo/index.html",{}) 
+    tasks=Task.objects.all()
+    return render(request,"todo/index.html",{'tasks':tasks})
