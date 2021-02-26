d511968edf50c3e96fc90a748756d7e08fe34f72
Ben <benjohn.mec@gmail.com>
2021-02-26 12:27:17 +0530

    adding base template html

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 import todo.urls
-from .views import home
+from .views import home,titlesubmit
 
 urlpatterns = [
-    path('',home,name="home")
+    path('',home,name="home"),
+    path('titlesubmit/',titlesubmit,name="titlesubmit"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
+import json
 from .models import *
 # Create your views here.
 
 def home(request):
     tasks=Task.objects.all()
     return render(request,"todo/index.html",{'tasks':tasks})
+
+def titlesubmit(request):
+    data=json.loads(request.body)
+    title=data['title']
+    print(title)
+    # t=Task()
+    # t.title=title
+    # t.save()
+    Task.objects.create(title=title)
+    return JsonResponse("success",safe=False)
